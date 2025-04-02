9bab33c2f827152107b4ac2f380b139ea6c29b21
wiscer-orga <wiscer.org@gmail.com>
2025-04-02 11:49:55 +1100

    Package: add babel to transpile

---
+++ dist/stepUrl.js
@@ -0,0 +1,9 @@
+/**
+ * Adjusts the numeric part of a URL by a given step.
+ * @param {string} currentUrl - The current URL.
+ * @param {number} step - The step to adjust the numeric part of the URL.
+ * @returns {string} - The updated URL.
+ */
+function stepUrl(currentUrl, step) {
+  return currentUrl.replace(/(\d+)(?=\D*$)/, (match) => parseInt(match) + step);
+}
