98d5c039cf95260920c3141e57eae5d94a693cea
wiscer-orga <wiscer.org@gmail.com>
2025-04-03 23:11:37 +1100

    temp update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import stepUrl from '../src/stepUrl';
 
-console.log('this is index.js');
+alert('update 6');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+
+    const currentUrl = window.location.href;
+    const prevUrl = stepUrl(currentUrl, -1);
+    const nextUrl = stepUrl(currentUrl, 1);
+    console.log('prevUrl', prevUrl);
+    console.log('nexxtUrl', nextUrl);
+
+    // const prevButton = document.getElementById('prevButton');
+    // const nextButton = document.getElementById('nextButton');
+    // console.log('prevButton', prevButton);
+    // console.log('nextButton', nextButton);
+});
+
+
+// document.getElementById('prevButton').onclick = function (e) {
+//     // e.preventDefault(); // Prevent the default action of the button
+//     console.log('prevButton clicked');
+//     const newUrl = stepUrl(-1);
+//     console.log('newUrl', newUrl);
+
+//     window.location.href = newUrl;
+// }
+
+// document.getElementById('nextButton').onclick = function (e) {
+//     // e.preventDefault(); // Prevent the default action of the button
+//     console.log('prevButton clicked');
+//     const newUrl = stepUrl(1);
+//     console.log('newUrl', newUrl);
+
+//     window.location.href = newUrl;
+// }
