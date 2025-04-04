6de077d1f6a1b447bba7360875e3aee754cba0ea
wiscer-orga <wiscer.org@gmail.com>
2025-04-04 13:10:59 +1100

    Fix: updated links must persist the digits length

--- csb/stepUrl.test.js
+++ csb/stepUrl.test.js
@@ -1,4 +1,4 @@
-const stepUrl = require('../src/stepUrl');
+const { stepUrl } = require('../src/nav');
 
 describe('stepUrl', () => {
   test('increments the numeric part of the URL', () => {
@@ -15,5 +15,9 @@ describe('stepUrl', () => {
 
   test('handles multiple numeric parts by adjusting the last one', () => {
     expect(stepUrl('/book/1/chapter/14', 1)).toBe('/book/1/chapter/15');
+  });
+
+  test('Maintain the number of digits in the URL', () => {
+    expect(stepUrl('/chapter/014', 1)).toBe('/chapter/015');
   });
 });
--- src/nav.js
+++ src/nav.js
@@ -5,7 +5,21 @@
  * @returns {string} - The updated URL.
  */
 function stepUrl(currentUrl, step) {
+  // Must persist the number of digits in the URL
+  const match = currentUrl.match(/(\d+)(?=\D*$)/);
+  if (match) {
+    const originalLength = match[0].length;
+    const updatedNumber = (parseInt(match[0]) + step).toString().padStart(originalLength, '0');
+    return currentUrl.replace(/(\d+)(?=\D*$)/, updatedNumber);
+  }
   return currentUrl.replace(/(\d+)(?=\D*$)/, (match) => parseInt(match) + step);
 }
 
-module.exports = stepUrl;
+function testFunc() {
+  console.log('testFunc called');
+}
+
+module.exports = {
+  stepUrl,
+  testFunc,
+};
