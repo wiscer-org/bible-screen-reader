a0a05403ea8df96ed1cf069a290041ba65a20255
wiscer-orga <wiscer.org@gmail.com>
2025-04-04 12:47:59 +1100

    Change the prev/next link on DOMContentLoaded

--- src/index.js
+++ src/index.js
@@ -1,37 +1,27 @@
-import stepUrl from '../src/stepUrl';
-
-alert('update 6');
+import stepUrl from './nav';
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOM fully loaded and parsed');
+    /**
+     * Navigate page by step (the number diff in the html file)
+     */
+    function navigateStepUrl(step) {
 
+    }
+    // Get the URLs to replace the navigation buttons
     const currentUrl = window.location.href;
     const prevUrl = stepUrl(currentUrl, -1);
     const nextUrl = stepUrl(currentUrl, 1);
+
+    // Get the buttons
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+
+    // Change the `href`in the `#prevButton` and `#nextButton` elements
+    prevButton.href = prevUrl;
+    nextButton.href = nextUrl;
+
+    // Log the URLs to the console for debugging
     console.log('prevUrl', prevUrl);
     console.log('nexxtUrl', nextUrl);
 
-    // const prevButton = document.getElementById('prevButton');
-    // const nextButton = document.getElementById('nextButton');
-    // console.log('prevButton', prevButton);
-    // console.log('nextButton', nextButton);
 });
-
-
-// document.getElementById('prevButton').onclick = function (e) {
-//     // e.preventDefault(); // Prevent the default action of the button
-//     console.log('prevButton clicked');
-//     const newUrl = stepUrl(-1);
-//     console.log('newUrl', newUrl);
-
-//     window.location.href = newUrl;
-// }
-
-// document.getElementById('nextButton').onclick = function (e) {
-//     // e.preventDefault(); // Prevent the default action of the button
-//     console.log('prevButton clicked');
-//     const newUrl = stepUrl(1);
-//     console.log('newUrl', newUrl);
-
-//     window.location.href = newUrl;
-// }
